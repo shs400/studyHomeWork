d9b4b5366f8977c8a4ec2db2440831668cfc6890
Hyun-Su SIM <hyunsu@Hyun-Suui-MacBook-Pro.local>
2017-02-05 12:48:08 +0900

    test

---
+++ src/app.js
@@ -0,0 +1,9 @@
+var math = require('./math');
+var $ = require('jquery');
+
+function a(){
+    return 11111;
+}
+
+console.log(a());
+console.log(math);
---
+++ src/math.js
@@ -0,0 +1,6 @@
+var math = {
+    max : function(){} ,
+    min : function(){}
+};
+
+module.exports = math;
